33f28f73f884cadfbf3e9cf31107efba303bbb64
Borisyu <hyyuag@connect.ust.hk>
2023-11-29 01:15:19 -0500

    Update summarizer.py

--- autofinviz/components/summarizer/summarizer.py
+++ autofinviz/components/summarizer/summarizer.py
@@ -1,26 +1,116 @@
 import pandas as pd
 from abc import ABC, abstractmethod
-from llmx import llm, TextGenerationConfig
+from openai import OpenAI
+import json
 
-class Summarizer(ABC):
+class Summarizer():
     def __init__(self) -> None:
         pass
 
-    def get_prompt(self) -> str:
+    def find_new_metrics(self, df: pd.DataFrame, df_name: str, category: str, gpt_client: OpenAI) -> list:
+
+        col = df.columns
+
+        if category == "Market Dataset":
+
+            system_prompt = "You are an experienced Market financial analyst that understand all market dataset, including Stock Price market, Commodity Price market and Currecy Exchange Price market."
+        
+        elif category == "Economic Dataset":
+
+            system_prompt = "You are an experienced Economist that understand all economic dataset, including Gross Domestic Product, Unemployment Rate and Consumer Price Index"
+
+        elif category == "Corporate Dataset":
+
+            system_prompt = " You are an experienced Corporate financial analyst that understand all company statement, including Income Statement and Cash Flow Statement."
+        
+        else:
+            return None
+
+        system_prompt += """
+        You can figure out the TOP3 metrics derived from the dataset.
+        TOP means each data row can calculate a new metric.
+        ONLY return the result in a form of "["name 1", "name 2", "name 3"]"
         """
-        Define prompt for enriching data summary. This is an abstract method and must be
-        implemented by all subclasses.
 
-        Returns:
-            str: Prompt for enriching data summary
+        message = f"""
+        I have a dataset called {df_name}, with its columns names below
+        {list(col)}
+        What are the TOP3 metrics I can derived from this dataset? (without explaination) 
         """
-        pass
 
-    def enriched_summary(self) -> str:
-        pass
+        completion = gpt_client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "assistant", "content": message},
+            ]
+        )
 
-    @classmethod
-    def base_summary(cls, df: pd.DataFrame, n_samples=3) -> list[dict]:
+        new_metrics = completion.choices[0].message.content
+        new_metrics = json.loads(new_metrics)
+
+        return new_metrics
+
+    def create_new_col(self, new_metrics: list, df: pd.DataFrame, df_name: str, category: str, gpt_client: OpenAI) -> str:
+
+        col = df.columns
+
+        if category == "Market Dataset":
+
+            system_prompt = """
+            You are an experienced Market financial analyst that understand all market dataset, including Stock Price market, Commodity Price market and Currecy Exchange Price market.
+            You know all the calculation of financial concpets.
+            """
+
+        elif category == "Economic Dataset":
+
+            system_prompt = """
+            You are an experienced Economist that understand all economic dataset, including Gross Domestic Product, Unemployment Rate and Consumer Price Index.
+            You know all the calculation of economic concpets.
+            """
+
+        elif category == "Corporate Dataset":
+            
+            system_prompt = """
+            You are an experienced Economist that understand all economic dataset, including Gross Domestic Product, Unemployment Rate and Consumer Price Index.
+            You know all the calculation of economic concpets.
+            """
+
+        else:
+            return None
+
+
+        system_prompt += """
+        i) ONLY return Executable Python code
+        ii) NO NEED TO read the dataset into a dataframe
+        iii) DO NOT print out the new values
+        iv) USE "df" to represent the dataframe in the code
+        """
+
+        message = f"""
+        I have a dataset called {df_name}, with its columns names below
+        {list(col)}
+        
+        Can you provide executable Python code to add the following columns
+        {new_metrics}
+
+        ONLY return Executable Python code
+        USE "df" to represent the dataframe in the code
+        Code must not require further input
+        
+        """
+
+        completion = gpt_client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "assistant", "content": message},
+        ])
+
+        return completion.choices[0].message.content
+
+
+    def base_summary(self, df: pd.DataFrame, n_samples=3) -> list[dict]:
         def get_samples(column):
             non_null = column.dropna().unique()
             return non_null[:n_samples].tolist()
@@ -59,20 +149,56 @@ class Summarizer(ABC):
         return properties_list
 
 
-    @classmethod
     def summarize(
-        self, data: pd.DataFrame,
-        generator: llm,
-        generation_config: TextGenerationConfig,
+        self, df: pd.DataFrame,
+        df_name: str, 
+        category: str,
+        gpt_client: OpenAI
+        # generator: llm,
+        # generation_config: TextGenerationConfig,
     ):
+
+        new_metrics = self.find_new_metrics(df, df_name, category, gpt_client)
+
+        print(new_metrics)
+
+        new_col_code = self.create_new_col(new_metrics, df, df_name, category, gpt_client)
+
+        print(new_col_code)
+
+        # self.__update_df(df, new_col_code)
         
-        base_summary = self.base_summary(data)
+        base_summary = self.base_summary(df)
         summary = {
             "dataset_description": "",
             "fields": base_summary,
         }
 
+        print(summary)
 
 
-        
+if __name__ == "__main__":
+
+    summarizer = Summarizer()
+
+    df = pd.read_csv("example/data/Stock_price_TSLA.csv")
+    df_name = "Stock_price_TSLA"
+    category = "Market Dataset"
+
+    def get_api_key():
+        try:
+            with open("apikey", 'r') as f:
+                return f.read().strip()
+        except FileNotFoundError:
+            print("'%s' file not found" % "apikey")
+
+    api_key = get_api_key()       
+
+    gpt_client = OpenAI(
+        api_key= api_key
+    )
+
+    summarizer.summarize(df, df_name, category, gpt_client)
+
+
         
