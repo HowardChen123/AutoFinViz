ade97dec1652fd3e2ed764e59ffc7168cd82bfe5
Borisyu1103 <hyyuag@connect.ust.hk>
2023-12-12 18:06:09 -0500

    RAG testing

---
+++ notebooks/graph/OHLC_chart.py
@@ -0,0 +1,12 @@
+## OHCL Chart
+import plotly.graph_objects as go
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+
+fig = go.Figure(data=go.Ohlc(x=df['Date'],
+                    open=df['Open'],
+                    high=df['High'],
+                    low=df['Low'],
+                    close=df['Close']))
+fig.show()
---
+++ notebooks/graph/Simple_candlestick.py
@@ -0,0 +1,15 @@
+## Simple Candlestick
+import plotly.graph_objects as go
+
+import pandas as pd
+from datetime import datetime
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+
+fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'])])
+
+fig.show()
