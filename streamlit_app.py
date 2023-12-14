c232c9b965b029d67ce9255eb7c6d30141673f2d
howardchen123 <howard.chenhc@gmail.com>
2023-12-14 17:09:29 -0500

    Reformat

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,28 +4,25 @@ import pandas as pd
 
 st.title('AutoFinViz')
 
-# OpenAI API Key Input
-openai_api_key = st.text_input('Enter your OpenAI API key', type='password')
+# Create a form for the OpenAI API Key Input
+with st.form("api_key_form"):
+    openai_api_key = st.text_input('Enter your OpenAI API key', type='password')
+    submit_key = st.form_submit_button('Submit API Key')
 
-# Save the API key in the config file if it's entered
-if openai_api_key:
+if submit_key and openai_api_key:
     openai.api_key = openai_api_key
     st.success('API Key saved!')
+else:
+    st.stop()
 
 # File Uploader
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
-# If a file is uploaded, read it into a DataFrame
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    # Display the DataFrame
-    st.write(df)
-
-# File Uploader
-# uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-
-# # If a file is uploaded, read it into a DataFrame
-# if uploaded_file is not None:
-#     df = pd.read_csv(uploaded_file)
-#     # Display the DataFrame
-#     st.write(df)
+    # Create a DataFrame and display it in a form
+    with st.form("dataframe_form"):
+        df = pd.read_csv(uploaded_file)
+        st.write(df)
+        st.form_submit_button('Confirm Data')
+else:
+    st.stop()
